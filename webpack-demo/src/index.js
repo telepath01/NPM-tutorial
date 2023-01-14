0561c184cc322d45592ffcc52592f64a32287fb3
Paul Rodriguez <paul.rodriguez03@gmail.com>
2023-01-13 20:00:07 -0500

    Working with NPM
    completed all required lessons for npm

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,25 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import printMe from './print.js';
+import myName from './myName';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   //   lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.textContent = myName('Paul');
+  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  //Add the image to our existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  element.appendChild(btn);
   return element;
 }
 document.body.appendChild(component());
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
